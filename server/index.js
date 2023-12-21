08d0746829b3c4127632b59650c29684cc91de72
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-12-21 16:24:46 +0400

    feat: add router

--- server/index.js
+++ server/index.js
@@ -3,16 +3,15 @@ const express = require('express');
 const models = require('./models/models')
 const sequelize = require('./db');
 const cors  = require('cors');
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: 'working'});
-})
 
 const start = async () => {
   try {
