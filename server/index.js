a5edea6959064ef308c0feaa1749d763c1299f11
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-12-21 19:22:18 +0400

    feat: add function for creating Type, Brand and Device in db

--- server/index.js
+++ server/index.js
@@ -3,14 +3,22 @@ const express = require('express');
 const models = require('./models/models')
 const sequelize = require('./db');
 const cors  = require('cors');
-const router = require('./routes/index')
+const fileUpload = require('express-fileupload');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
+
+// обработка ошибок, последний Middleware
+app.use(errorHandler);
 
 
 const start = async () => {
