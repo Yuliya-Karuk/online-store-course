a5f80aec7237e688d59241bb5147850ae38801bb
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-12-21 21:21:04 +0400

    feat: add function for return device by id

--- server/models/models.js
+++ server/models/models.js
@@ -67,7 +67,7 @@ Device.belongsTo(Brand);
 Device.hasMany(Rating);
 Rating.belongsTo(Device);
 
-Device.hasMany(DeviceInfo);
+Device.hasMany(DeviceInfo, {as: 'info'});
 DeviceInfo.belongsTo(Device);
 
 Device.hasMany(BasketDevise);
