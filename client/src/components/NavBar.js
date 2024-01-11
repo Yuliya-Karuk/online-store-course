6c30ecbb945c814366613f0d3e1fbd862213e137
Yuliya-Karuk <karuk.yuliya@gmail.com>
2024-01-11 20:43:47 +0400

    feat: implement AdminPage

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,20 +4,22 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
-import { SHOP_ROUTE } from '../utils/consts';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { observer } from 'mobx-react-lite';
 
 const NavBar = observer(() => {
   const {user} = useContext(Context);
+  const history = useNavigate();
+
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
         <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>BuyDevice</NavLink>
         {user.isAuth ?
           <Nav className="ml-auto" style={{color: 'white'}}>
-            <Button variant={"outline-light"} className="me-2">Admin panel</Button>
-            <Button variant={"outline-light"}>Log out</Button>
+            <Button variant={"outline-light"} onClick={() => history(ADMIN_ROUTE)} className="me-2">Admin panel</Button>
+            <Button variant={"outline-light"} onClick={() => history(LOGIN_ROUTE)}>Log out</Button>
           </Nav>
           :
           <Nav className="ml-auto" style={{color: 'white'}}>
---
+++ client/src/components/modals/CreateBrand.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+const CreateBrand = ({show, onHide}) => {
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add Brand
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Control
+            placeholder={'Enter type name'}
+          />
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='outline-danger' onClick={onHide}>Close</Button>
+        <Button variant='outline-success' onClick={onHide}>Add</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default CreateBrand;
