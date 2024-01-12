dedd3e20d2eb4fd0def510523edd8596c1332f2d
Yuliya-Karuk <karuk.yuliya@gmail.com>
2024-01-12 17:06:07 +0400

    feat: implement functions for get data from server

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
@@ -6,21 +6,22 @@ import Row from 'react-bootstrap/Row';
 import bigStar from '../assets/big_star.png';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { useParams } from 'react-router-dom';
+import { fetchOneDevice } from '../http/deviceAPI';
 
 const DevicePage = () => {
-  const device = {id: 1, name: 'Iphone 12 pro', price: 100000, rating: 5, img: 'https://ir.ozone.ru/s3/multimedia-c/wc1000/6765094956.jpg'};
-  const description = [
-    {id: 1, title: 'Random access memory', description: '5 G'},
-    {id: 2, title: 'Camera', description: '12 Mn'},
-    {id: 3, title: 'Processor', description: 'Exynos'},
-    {id: 4, title: 'Number of Cores', description: '2'},
-    {id: 5, title: 'Battery', description: '4000'},
-  ]
+  const [device, setDevice] = useState({info: []});
+  const {id} = useParams();
+
+  useEffect(() => {
+    fetchOneDevice(id).then(data =>setDevice(data))
+  }, [])
+
   return (
     <Container className='mt-3'>
       <Row>
         <Col md={4}>
-          <Image width={300} height={300} src={device.img} style={{objectFit: 'contain'}} />
+          <Image width={300} height={300} src={process.env.REACT_APP_API_URL + device.img} style={{objectFit: 'contain'}} />
         </Col>
         <Col md={4}>
           <Row className='d-flex flex-column align-items-center'>
@@ -45,7 +46,7 @@ const DevicePage = () => {
       </Row>
       <Row className='d-flex flex-column m-3'>
         <h1>Specifications</h1>
-        {description.map((info, index) =>
+        {device.info.map((info, index) =>
           <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
             {info.title}:{info.description}
           </Row>
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -2,26 +2,9 @@ import {makeAutoObservable} from "mobx";
 
 export default class DeviceStore {
   constructor() {
-    this._types = [
-      {id: 1, name: 'холодильники'},
-      {id: 2, name: 'смартфоны'},
-      {id: 3, name: 'ноутбуки'},
-    ]
-    this._brands = [
-      {id: 1, name: 'Samsung'},
-      {id: 2, name: 'Apple'},
-      {id: 3, name: 'Atlant'},
-    ]
-    this._devices = [
-      {id: 1, name: 'Iphone 12 pro', price: 100000, rating: 5, img: 'https://ir.ozone.ru/s3/multimedia-c/wc1000/6765094956.jpg'},
-      {id: 2, name: 'Iphone 13 pro', price: 100000, rating: 5, img: 'https://ir.ozone.ru/s3/multimedia-c/wc1000/6765094956.jpg'},
-      {id: 3, name: 'Iphone 14 pro', price: 100000, rating: 5, img: 'https://ir.ozone.ru/s3/multimedia-c/wc1000/6765094956.jpg'},
-      {id: 4, name: 'Iphone 15 pro', price: 100000, rating: 5, img: 'https://ir.ozone.ru/s3/multimedia-c/wc1000/6765094956.jpg'},
-      {id: 5, name: 'Iphone 12 pro', price: 100000, rating: 5, img: 'https://ir.ozone.ru/s3/multimedia-c/wc1000/6765094956.jpg'},
-      {id: 6, name: 'Iphone 13 pro', price: 100000, rating: 5, img: 'https://ir.ozone.ru/s3/multimedia-c/wc1000/6765094956.jpg'},
-      {id: 7, name: 'Iphone 14 pro', price: 100000, rating: 5, img: 'https://ir.ozone.ru/s3/multimedia-c/wc1000/6765094956.jpg'},
-      {id: 8, name: 'Iphone 15 pro', price: 100000, rating: 5, img: 'https://ir.ozone.ru/s3/multimedia-c/wc1000/6765094956.jpg'},
-    ]
+    this._types = [];
+    this._brands = [];
+    this._devices = [];
     this._selectedType = {};
     this._selectedBrand = {};
     makeAutoObservable(this);
