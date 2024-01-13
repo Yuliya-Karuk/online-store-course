cff75cfb37b731a88feba03025a20f405fc41613
Yuliya-Karuk <karuk.yuliya@gmail.com>
2024-01-13 20:27:47 +0400

    fix: remove commented code

--- client/src/components/BrandBar.js
+++ client/src/components/BrandBar.js
@@ -1,7 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { useContext } from 'react';
 import { Context } from '..';
-import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 
@@ -9,21 +8,6 @@ const BrandBar = observer(() => {
   const {device} = useContext(Context);
 
   return (
-    // <Row className='d-flex'>
-    //   {device.brands.map(brand =>
-    //     <Col className='p-0' xs lg="2">
-    //       <Card
-    //         key={brand.id}
-    //         style={{cursor: 'pointer'}}
-    //         border={brand.id === device.selectedBrand.id ? 'danger' : '--bs-border-color'}
-    //         onClick={() => device.setSelectedBrand(brand)}
-    //         className='p-2'
-    //       >
-    //         {brand.name}
-    //       </Card>
-    //     </Col>
-    //   )}
-    // </Row>
     <Col className='d-flex gap-2'>
       {device.brands.map(brand =>
           <Card
